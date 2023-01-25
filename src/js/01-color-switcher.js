b187f3cb0d9af13bfe7c85456ad58ae10eef4ff6
EwaMazurek <ewa.mmazurek@gmail.com>
2023-01-25 20:34:23 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timer = null;
+
+startBtn.addEventListener('click', e => {
+  document.querySelector('body').style.backgroundColor = getRandomHexColor();
+  timer = setInterval(() => {
+    document.querySelector('body').style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  e.currentTarget.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', e => {
+  clearInterval(timer);
+  startBtn.disabled = false;
+  e.currentTarget.disabled = true;
+});
